f55719028533ec20fca590b6d8456c289ffeeafc
farisridzal-hftm <faris.ridzal@hftm.ch>
2026-06-13 11:02:16 +0200

    feat: add blog overview and likeable blog cards

---
+++ src/app/feature/blog/blog.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlogOverview } from './blog';
+
+describe('BlogOverview', () => {
+  let component: BlogOverview;
+  let fixture: ComponentFixture<BlogOverview>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BlogOverview],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BlogOverview);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/blog-card/blog-card.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlogCard } from './blog-card';
+import { Blog } from '../models/blog.interface';
+
+describe('BlogCard', () => {
+  let component: BlogCard;
+  let fixture: ComponentFixture<BlogCard>;
+
+  const mockBlog: Blog = {
+    id: 1,
+    title: 'Test Titel',
+    contentPreview: 'Test Vorschau',
+    author: 'Test Autor',
+    likes: 0,
+    comments: 0,
+    likedByMe: false,
+    createdByMe: false,
+    createdAt: '2026-02-15T10:30:00',
+    updatedAt: '2026-02-16T08:15:00',
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BlogCard],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BlogCard);
+    fixture.componentRef.setInput('blog', mockBlog);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
