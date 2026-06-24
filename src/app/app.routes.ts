55ee8a7e6c065ef0e7afb89223c915349537fd60
farisridzal-hftm <faris.ridzal@hftm.ch>
2026-06-24 20:22:01 +0200

    fix: resolve merge conflicts with main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-
 import { blogDetailResolver } from './feature/blog-detail/blog-detail.resolver';
 import { BlogOverview } from './feature/blog-overview/blog-overview';
 
